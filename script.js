292670457a18bc6b5273575b655280758bfba75e
dedge-ct <tuquerojedric@gmail.com>
2024-10-30 12:14:59 +0800

    Added menu functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var isSequential = false;
+var themeCounter = 0;
 
 function changeView() {
     var btn = document.getElementById("change-view");
@@ -11,4 +12,29 @@ function changeView() {
         isSequential = false;
     }
 
+}
+
+function openOrderMenu() {
+    document.getElementById("article-order-menu").style.display = "inline";
+}
+
+function changeTheme() {
+    var themeButton = document.getElementById("change-theme");
+    themeCounter++;
+    switch (themeCounter) {
+        case 1:
+            themeButton.innerHTML = "Theme: Cascade";
+            break;
+        case 2:
+            themeButton.innerHTML = "Theme: Soundch";
+            break;
+        default:
+            themeCounter = 0;
+            themeButton.innerHTML = "Theme: Vegas"
+            break;
+    }
+}
+
+function reorderArticles() {
+    document.getElementById("article-order-menu").style.display = "none";
 }

77ac994e3f92b7da802de69bad57bb8ca94cb36a
dedge-ct <tuquerojedric@gmail.com>
2024-10-30 17:59:16 +0800

    Fixed article layout

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function changeView() {
     var btn = document.getElementById("change-view");
     if (!isSequential) {
         btn.innerHTML = "Layout: Sequential";
+        changeToSequential();
         isSequential = true;
     }
     else {
@@ -36,5 +37,25 @@ function changeTheme() {
 }
 
 function reorderArticles() {
+    const selectedArticle1 = document.getElementById("select-article-1").value;
+    const selectedArticle2 = document.getElementById("select-article-2").value;
+    const selectedArticle3 = document.getElementById("select-article-3").value;
+    const selectedArticle4 = document.getElementById("select-article-4").value;
+    const selectedArticle5 = document.getElementById("select-article-5").value;
+    const selectedArticle6 = document.getElementById("select-article-6").value;
+    const selectedArticleArray = [selectedArticle1, selectedArticle2, selectedArticle3, selectedArticle4, selectedArticle5, selectedArticle6];
+    const selectedArticleSet = new Set(selectedArticleArray);
+    if (selectedArticleSet.size != selectedArticleArray.length) {
+        alert("Two or more of the same articles were selected. Please try again.");
+        return 0;
+    }
     document.getElementById("article-order-menu").style.display = "none";
+}
+
+function changeToSequential() {
+    articles = document.getElementsByClassName("articles");
+    for (let i = 0; i < articles.length; i++) {
+        articles[i].style.width = "100%";
+        articles[i].style.margin = "5px";
+    }
 }
